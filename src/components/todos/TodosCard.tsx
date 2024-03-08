baeb2e26b3d3dab05bb1ef5fa2347e19e91e359b
mominul <ulhouqemomin39@gmail.com>
2024-03-08 16:08:38 +0600

    added toggled todo pending or done

--- src/components/todos/TodosCard.tsx
+++ src/components/todos/TodosCard.tsx
@@ -1,25 +1,40 @@
 import { Edit, Trash } from "lucide-react";
 import { Button } from "../ui/button";
 import { useAppDispatch } from "@/redux/hooks";
-import { removeTodo } from "@/redux/Features/todoSlice";
+import { removeTodo, toggleComplete } from "@/redux/Features/todoSlice";
 
 type TTodoCardProps = {
-  id:string
+  id: string;
   title: string;
   description: string;
+  isCompleted?: boolean;
 };
 
-const TodosCard = ({ title, description,id }:TTodoCardProps) => {
+const TodosCard = ({ title, description, id, isCompleted }: TTodoCardProps) => {
+  const dispatch = useAppDispatch();
 
-  const dispatch =useAppDispatch()
+  const toggleState = () => {
+    dispatch(toggleComplete(id));
+  };
   return (
     <div className="bg-white p-2 rounded-md flex justify-between items-center w-full border">
-      <input type="checkbox" name="" id="" />
+      <input onChange={toggleState} type="checkbox" name="" id="" />
       <p className="font-semibold">{title}</p>
       {/* <p>Time</p> */}
+      <div>
+        {isCompleted ? (
+          <p className="text-green-500">Done</p>
+        ) : (
+          <p className="text-red-500">Pending</p>
+        )}
+      </div>
       <p>{description}</p>
       <div className="space-x-2">
-        <Button onClick={()=>dispatch(removeTodo(id))} size={"sm"} className="bg-red-600">
+        <Button
+          onClick={() => dispatch(removeTodo(id))}
+          size={"sm"}
+          className="bg-red-600"
+        >
           <Trash size={18} />
         </Button>
         <Button size={"sm"} className="bg-blue-500">
--- src/components/todos/TodosContainer.tsx
+++ src/components/todos/TodosContainer.tsx
@@ -15,13 +15,9 @@ const TodosContainer = () => {
 
       <div className="bg-primary-gradient p-[5px] rounded-xl w-full h-auto ">
         <div className="bg-white p-4 w-full h-full rounded-lg space-y-2">
-       {
-        todos.map((items)=>(
-          <TodosCard {...items}
-          />
-        ))
-       }
-
+          {todos.map((items) => (
+            <TodosCard {...items} />
+          ))}
         </div>
       </div>
     </div>
